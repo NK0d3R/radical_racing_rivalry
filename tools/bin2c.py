5740feb6a17801670634ea81543231da1413a929
Catalin G. Manciu <mr.catalin@gmail.com>
2018-02-23 23:21:56 -0800

    bin2c.py outputs spaces instead of tabs

--- tools/bin2c.py
+++ tools/bin2c.py
@@ -3,15 +3,16 @@ import sys
 import os
 
 def main():
-
     parser = argparse.ArgumentParser(
-                                description="Binary to C declaration converter")
+                        description="Binary to C declaration converter")
     parser.add_argument("-i", "--input", help="Input file to process",
                         required=True)
     parser.add_argument("-o", "--output", help="Output C file (no ext)",
                         required=True)
     parser.add_argument("-m", "--maxvals", help="Max number of values per line",
                         type=int, default=16, required=False)
+    parser.add_argument("-t", "--tabs", help="Tab size in spaces",
+                        type=int, default=4, required=False)
     options = parser.parse_args()
 
     buffer_size = 2048
@@ -29,15 +30,16 @@ def main():
     variable_name_base = basename.upper().replace(" ", "_").replace(".","_")
     variable_name_size = variable_name_base + "_SIZE"
     variable_name_data = variable_name_base + "_DATA"
+    tab_str = " " * options.tabs
 
     with open(options.input, "rb") as input:
         with open(options.output, "wt") as output:
             output.write("#ifndef __%s_H__\n#define __%s_H__\n\n" %
                          (variable_name_base, variable_name_base))
-            output.write("#define %s\t%d\n" % (
-                         variable_name_size, file_size))
-            output.write("PROGMEM const uint8_t %s[] = {\n\t" %
-                         variable_name_data)
+            output.write("#define %s%s%d\n" % (
+                         variable_name_size, tab_str, file_size))
+            output.write("PROGMEM const uint8_t %s[] = {\n%s" %
+                         (variable_name_data, tab_str))
 
             crt_line_byte = 0
             new_line_finish = True
@@ -50,14 +52,15 @@ def main():
                                      format(ord(byte), "#04x"))
                         crt_line_byte += 1
                         if crt_line_byte == options.maxvals:
-                            output.write("\n\t")
+                            output.write("\n%s" % (tab_str))
                             crt_line_byte = 0
                             new_line_finish = True
                 else:
                     break
                 if new_line_finish is False:
                     output.write("\n")
-                output.write("};\n\n#endif //__%s_H__\n" % (variable_name_base))
+                output.write("\n};\n\n#endif //__%s_H__\n" %
+                             (variable_name_base))
 
 if __name__ == "__main__":
     main()
