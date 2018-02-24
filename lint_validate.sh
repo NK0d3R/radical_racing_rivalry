819a24e76352d5b569566402ec79ba2ff92e7c9d
Catalin G. Manciu <mr.catalin@gmail.com>
2018-02-23 23:39:35 -0800

    Refactor: added Google cpplint.py

---
+++ lint_validate.sh
@@ -0,0 +1,25 @@
+echo Checking source files
+PROJECT_FOLDER=car_game
+TOOLS_FOLDER=tools
+GOOGLE_LINT=$TOOLS_FOLDER/third-party/cpplint/cpplint.py
+for filename in $PROJECT_FOLDER/*; do
+    OUT=`git diff $filename`
+    if [ ! -z "$OUT" ]; then
+        echo "$filename was updated, checking"
+        while :
+        do
+            python $GOOGLE_LINT --root=$PROJECT_FOLDER $filename
+            if [ $? == 0 ]; then
+                echo File is OK !
+                break
+            else
+                echo "Errors found ! (r)etry or (n)ext"
+                read res
+                if [ $res == "n" ]; then
+                    break
+                fi
+            fi
+        done
+    fi
+done
+echo Done
