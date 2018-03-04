856b5b0319b5a4c2016969058817b99d3263d2f9
Catalin G. Manciu <mr.catalin@gmail.com>
2018-03-04 01:57:37 -0800

    Update lint_validate
    Lint_validate now checks all files regardless of git diff output.
    Filtered include-related error, they're very Google-specific.

--- lint_validate.sh
+++ lint_validate.sh
@@ -3,23 +3,25 @@ PROJECT_FOLDER=car_game
 TOOLS_FOLDER=tools
 GOOGLE_LINT=$TOOLS_FOLDER/third-party/cpplint/cpplint.py
 for filename in $PROJECT_FOLDER/*; do
-    OUT=`git diff $filename`
-    if [ ! -z "$OUT" ]; then
-        echo "$filename was updated, checking"
-        while :
-        do
-            python $GOOGLE_LINT --repository="$PROJECT_FOLDER" $filename
-            if [ $? == 0 ]; then
-                echo File is OK !
+    echo "Checking $filename"
+    while :
+    do
+        python $GOOGLE_LINT --root="$PROJECT_FOLDER"                    \
+                --repository="$PROJECT_FOLDER" --filter=-build/include  \
+                $filename
+        if [ $? == 0 ]; then
+            echo File is OK !
+            break
+        else
+            echo "Errors found ! (r)etry (n)ext (q)uit"
+            read res
+            if [ $res == "n" ]; then
                 break
-            else
-                echo "Errors found ! (r)etry or (n)ext"
-                read res
-                if [ $res == "n" ]; then
-                    break
-                fi
             fi
-        done
-    fi
+            if [ $res == "q" ]; then
+                exit 255
+            fi
+        fi
+    done
 done
 echo Done
