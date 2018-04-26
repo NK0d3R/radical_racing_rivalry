067b2c7cb579390a800a2c6d5ffd32c3dbec8726
Catalin G. Manciu <mr.catalin@gmail.com>
2018-04-25 22:52:20 -0700

    Fix res path for do_gfx.sh

--- do_gfx.sh
+++ do_gfx.sh
@@ -1,12 +1,13 @@
 echo --- GFX ---
 PROJECT_FOLDER=car_game
+RES_FOLDER=$PROJECT_FOLDER/src/res
 GFX_FOLDER=gfx
 TOOLS_FOLDER=tools
 COPYRIGHT_FILE=$TOOLS_FOLDER/copyright.inc
 
-python $TOOLS_FOLDER/bin2c.py -i $GFX_FOLDER/environment.abs -o $PROJECT_FOLDER/env_sprite.h -m 8 -c $COPYRIGHT_FILE
-python $TOOLS_FOLDER/bin2c.py -i $GFX_FOLDER/car.abs -o $PROJECT_FOLDER/car_sprite.h -m 8 -c $COPYRIGHT_FILE
-python $TOOLS_FOLDER/bin2c.py -i $GFX_FOLDER/font.abs -o $PROJECT_FOLDER/font.h -m 8 -c $COPYRIGHT_FILE
-python $TOOLS_FOLDER/fontmap.py -i $GFX_FOLDER/fontmap.txt -o $PROJECT_FOLDER/fontmap.h -c $COPYRIGHT_FILE
+python $TOOLS_FOLDER/bin2c.py -i $GFX_FOLDER/environment.abs -o $RES_FOLDER/env_sprite.h -m 8 -c $COPYRIGHT_FILE
+python $TOOLS_FOLDER/bin2c.py -i $GFX_FOLDER/car.abs -o $RES_FOLDER/car_sprite.h -m 8 -c $COPYRIGHT_FILE
+python $TOOLS_FOLDER/bin2c.py -i $GFX_FOLDER/font.abs -o $RES_FOLDER/font.h -m 8 -c $COPYRIGHT_FILE
+python $TOOLS_FOLDER/fontmap.py -i $GFX_FOLDER/fontmap.txt -o $RES_FOLDER/fontmap.h -c $COPYRIGHT_FILE
 
 echo Done
